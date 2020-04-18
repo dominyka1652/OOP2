e71de3de1e93feeb2afa36c7cf639aecb50f7ffd
dominyka1652 <57347384+dominyka1652@users.noreply.github.com>
2020-04-18 13:40:37 +0300

    Add files via upload

---
+++ setget.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+
+void Studentas::setVardas(std::string vardas_) {
+	vardas = vardas_;
+}
+
+void Studentas::setPavarde(std::string pavarde_) {
+	pavarde = pavarde_;
+}
+void Studentas::setEgzaminas(int egzaminas_) {
+	egzaminas = egzaminas_;
+}
